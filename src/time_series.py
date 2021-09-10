dfb6a5fc03d1affb7f465cbd10963f9ede357231
carlaterboven <carla.terboven@student.hpi.de>
2021-09-10 23:21:13 +0200

    Clean up code

--- src/time_series.py
+++ src/time_series.py
@@ -5,7 +5,7 @@ import os
 from generate_sound import *
 
 dirname = os.path.dirname(__file__)
-DATA_FILE = os.path.join(dirname, '../data/ride4.csv')
+DATA_FILE = os.path.join(dirname, '../data/ride6.csv')
 ride_df = pd.read_csv(DATA_FILE)
 sampling_rate = 10
 
@@ -34,7 +34,7 @@ if __name__ ==  '__main__':
             joint_pm2_5 = (joint_pm2_5/sampling_rate)
             joint_pm10 = (joint_pm10/sampling_rate)
 
-            # # version1
+            # # concept1
             # p1 = Process(target=breath, args=[pm1])
             # p2 = Process(target=geiger_counter, args=[pm10s, joint_pm10])
             # p1.start()
@@ -42,18 +42,18 @@ if __name__ ==  '__main__':
             # p1.join()
             # p2.join()
 
-            # # version2
-            p1 = Process(target=breath, args=[pm1])
-            p2 = Process(target=air_bubbles, args=[pm2_5])
-            p3 = Process(target=asthma_inhaler, args=[pm10s, joint_pm10])
-            p1.start()
-            p2.start()
-            p3.start()
-            p1.join()
-            p2.join()
-            p3.join()
+            # # concept2
+            # p1 = Process(target=breath, args=[pm1])
+            # p2 = Process(target=air_bubbles, args=[pm2_5])
+            # p3 = Process(target=asthma_inhaler, args=[pm10s, joint_pm10])
+            # p1.start()
+            # p2.start()
+            # p3.start()
+            # p1.join()
+            # p2.join()
+            # p3.join()
 
-            # # version3
+            # # concept3
             # p1 = Process(target=wind_chimes, args=[joint_pm2_5, joint_pm10])
             # p2 = Process(target=wind_leaves, args=[joint_pm2_5, joint_pm10])
             # p3 = Process(target=wind, args=[joint_pm2_5, joint_pm10])
@@ -64,18 +64,18 @@ if __name__ ==  '__main__':
             # p2.join()
             # p3.join()
 
-            # # version4
-            # p1 = Process(target=bees, args=[joint_pm2_5])
-            # p2 = Process(target=birds, args=[joint_pm10])
-            # p3 = Process(target=bird_alarm, args=[joint_pm2_5, joint_pm10])
-            # p1.start()
-            # p2.start()
-            # p3.start()
-            # p1.join()
-            # p2.join()
-            # p3.join()
+            # concept4
+            p1 = Process(target=bees, args=[joint_pm2_5])
+            p2 = Process(target=birds, args=[joint_pm10])
+            p3 = Process(target=bird_alarm, args=[joint_pm2_5, joint_pm10])
+            p1.start()
+            p2.start()
+            p3.start()
+            p1.join()
+            p2.join()
+            p3.join()
 
-            # # version5
+            # # concept5
             # # first time
             # if index == sampling_rate:
             #     music()
