a14b9ee902c157048d11c942ca56473888420dca
carlaterboven <carla.terboven@student.hpi.de>
2022-02-22 16:30:10 +0100

    Undo last change

--- src/interactive.py
+++ src/interactive.py
@@ -1,78 +1,82 @@
 # coding=utf-8
 import RPi.GPIO as GPIO
-from enum import Enum
-import pandas as pd
 import time
-from multiprocessing import Process
-import os
-from generate_sound import *
-import sensor as sensor
-import rotary_knob as knob
-import sound_concept as sound_concept
+from enum import Enum
 
-if __name__ ==  '__main__':
-    print("Read and sonify PM data. Change sonification mode with rotary knob. [Press Ctrl+C to exit!]")
-    GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BCM)
 
-    rotary = knob.Rotaryknob()
-    sensor = sensor.Sensor()
+# declare pins of rotary encoder
+PIN_CLK = 16
+PIN_DT = 7
+BUTTON_PIN = 8
 
-    GPIO.setup(rotary.PIN_CLK, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-    GPIO.setup(rotary.PIN_DT, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-    GPIO.setup(rotary.BUTTON_PIN, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(PIN_CLK, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(PIN_DT, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 
-    rotary.set_clk_last(GPIO.input(rotary.PIN_CLK))
+# initialize
+counter = 0
+direction = True
+# PIN_CLK_LAST = 0
+PIN_CLK_LAST = GPIO.input(PIN_CLK)
+PIN_CLK_NEW = 0
+delayTime = 0.01
+# number of steps of the rotary encoder
+ROTARYSTEPS = 20
 
-    # To directly integrate a debounce, the functions for output are initialized by the CallBack-Option of the GPIO Python module
-    GPIO.add_event_detect(rotary.PIN_CLK, GPIO.BOTH, callback=rotary.turn_knob, bouncetime=50)
-    GPIO.add_event_detect(rotary.BUTTON_PIN, GPIO.FALLING, callback=rotary.counter_reset, bouncetime=50)
+class Concept(Enum):
+    TEST = 0
+    GEIGER = 1
+    ASTHMA = 2
+    WIND = 3
+    BEES = 4
 
-    first_round = True
-    processes = []
+def get_concept(rotary_position):
+    if (rotary_position >= 3 and rotary_position < 8):
+        concept = Concept.BEES
+    elif (rotary_position >= 8 and rotary_position < 13):
+        concept = Concept.WIND
+    elif (rotary_position >= 13 and rotary_position < 18):
+        concept = Concept.ASTHMA
+    else:
+        concept = Concept.GEIGER
+    return concept
 
-    try:
+# output function runs if sigal detected
+def rotary_output(null):
+    global counter
+    PIN_CLK_NEW = GPIO.input(PIN_CLK)
+    if PIN_CLK_NEW != PIN_CLK_LAST:
+        if GPIO.input(PIN_DT) != PIN_CLK_NEW:
+            counter += 1
+        else:
+            counter -= 1
+
+        print("New Position: ", counter)
+        print("------------------------------")
+
+        rotary_position = counter%ROTARYSTEPS
+        concept = get_concept(rotary_position)
+
+        print("New Concept: ", concept)
+        print("--------------------------------")
+
+def counter_reset(null):
+    global counter
+    print("Reset Position!")
+    print("------------------------------")
+    counter = 0
+
+# Um einen Debounce direkt zu integrieren, werden die Funktionen zur Ausgabe mittels
+# CallBack-Option vom GPIO Python Modul initialisiert
+GPIO.add_event_detect(PIN_CLK, GPIO.BOTH, callback=rotary_output, bouncetime=50)
+GPIO.add_event_detect(BUTTON_PIN, GPIO.FALLING, callback=counter_reset, bouncetime=50)
+
+print("Rotary-Encoder Test [Press Ctrl+C to exit!]")
+
+try:
         while True:
-            if first_round:
-                # first time: collect data 10s without sonification
-                start_sound()
-            else:
-                # any other time: prepare data and start sonification processes
-                sensor.prepare_data()
-                sound_concept_object = sound_concept.SoundConcept(sensor.get_pm1(), sensor.get_pm2_5(), sensor.get_pm10(), sensor.get_joint_pm2_5(), sensor.get_joint_pm10(), sensor.get_pm10s())
+            time.sleep(delayTime)
 
-                # setup processes based on mode
-                mode = rotary.get_mode()
-                concept = sound_concept_object.get_concept(mode.name)
-                processes.clear()
-                for part in concept:
-                    processes.append(Process(target=concept[part].target, args=concept[part].args))
-                for process in processes:
-                    process.start()
-                    print("first check: ", process.is_alive())
-
-            sensor.reset_data()
-
-            start_timer = time.time()
-            while True:
-                sensor.read_data()
-
-                if time.time() - start_timer > 10:
-                # if time.time() - start_timer > 10 and p1.is_alive():
-                    break
-
-                if not first_round:
-                    alive = True
-                    for process in processes:
-                        print("alive: ", alive)
-                        alive = alive and process.is_alive()
-                    if not alive:
-                        break
-
-            if first_round:
-                first_round = False
-            else:
-                for process in processes:
-                    process.join()
-
-    except KeyboardInterrupt:
-            GPIO.cleanup()
+except KeyboardInterrupt:
+        GPIO.cleanup()
